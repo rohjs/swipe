c7c4cd088da61d54171c61a069307542690c8e92
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-12-10 23:01:16 +0900

    Delete example component and import components/Button

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,38 +1,30 @@
-import React from 'react'
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0'
+import { Meta } from '@storybook/react/types-6-0'
 
-import { Button, ButtonProps } from './Button'
+import { Button } from '../components/Button'
+import { createThemedTemplate } from './utils/theme'
+
+const { Template, themeArgType } = createThemedTemplate(Button)
 
 export default {
   title: 'Example/Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' }
+    backgroundColor: { control: 'color' },
+    theme: themeArgType
   }
 } as Meta
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />
-
 export const Primary = Template.bind({})
 Primary.args = {
-  primary: true,
-  label: 'Button'
+  variant: 'primary',
+  size: 'big',
+  fullWidth: true,
+  children: 'hi'
 }
 
 export const Secondary = Template.bind({})
 Secondary.args = {
-  label: 'Button'
-}
-
-export const Large = Template.bind({})
-Large.args = {
-  size: 'large',
-  label: 'Button'
-}
-
-export const Small = Template.bind({})
-Small.args = {
-  size: 'small',
-  label: 'Button'
+  variant: 'secondary',
+  size: 'medium',
+  children: 'hi'
 }
