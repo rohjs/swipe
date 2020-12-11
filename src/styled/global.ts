fb2455df1de0d200c4f1317862891815220d274a
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-12-12 08:31:30 +0900

    Delete bodyStyle

--- src/styled/global.ts
+++ src/styled/global.ts
@@ -1,20 +1,9 @@
-import { createGlobalStyle, css } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+import styledReset from 'styled-reset'
 import { lightThemes } from './theme'
 
-export const fontUrl =
-  'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900'
-
-export const bodyStyles = css`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    font-family: Inter, --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-      Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    color: ${lightThemes.colors.grayscale.primary};
-  }
-`
-
 export const GlobalStyle = createGlobalStyle`
+  ${styledReset}
   * {
     box-sizing: border-box;
     margin: 0;
